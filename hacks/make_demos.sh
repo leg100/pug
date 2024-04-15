1962ceb254d99d29dab8f004cbd68e77e56baf24
Louis Garman <louisgarman@gmail.com>
2024-04-15 12:10:15 +0100

    demos

--- hacks/make_demos.sh
+++ hacks/make_demos.sh
@@ -1,3 +1,3 @@
 vhs demos/modules.tape
 MODULES_GIF_URL=$(vhs publish demos/modules.gif)
-sed -i -e "s|\(\!\[Modules demo\]\).*|\1$MODULES_GIF_URL|" README.md
+sed -i -e "s|\(\!\[Modules demo\]\).*|\1($MODULES_GIF_URL)|" README.md
