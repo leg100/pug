eb27209f7d0660504693961587aa44c74becbb0c
Louis Garman <louisgarman@gmail.com>
2024-05-13 21:26:26 +0100

    fix: show version when using go install

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,6 +1,27 @@
 package version
 
+import "runtime/debug"
+
 var (
 	// Build-time parameters set via -ldflags
 	Version = "unknown"
 )
+
+// A user may install pug using `go install github.com/leg100/pug@latest`
+// without -ldflags, in which case the version above is unset. As a workaround
+// we use the embedded build version that *is* set when using `go install` (and
+// is only set for `go install` and not for `go build`).
+func init() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		// < go v1.18
+		return
+	}
+	mainVersion := info.Main.Version
+	if mainVersion == "" || mainVersion == "(devel)" {
+		// bin not built using `go install`
+		return
+	}
+	// bin built using `go install`
+	Version = mainVersion
+}
