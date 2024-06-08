26cd0cc2442b79a605c258135844349c93d36bb9
Louis Garman <louisgarman@gmail.com>
2024-06-08 10:53:31 +0100

    wip

--- internal/tui/cache.go
+++ internal/tui/cache.go
@@ -29,16 +29,6 @@ func (c *Cache) Exists(page Page) bool {
 	return ok
 }
 
-func (c *Cache) Entries() []resource.ID {
-	ids := make([]resource.ID, len(c.cache))
-	var i int
-	for k := range c.cache {
-		ids[i] = k.id
-		i++
-	}
-	return ids
-}
-
 func (c *Cache) Get(page Page) tea.Model {
 	return c.cache[NewCacheKey(page)]
 }
