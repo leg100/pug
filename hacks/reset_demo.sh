dbb845870cde3fc1c518805581119994962bc482
Louis Garman <louisgarman@gmail.com>
2024-06-21 14:35:19 +0100

    chore: update demo

--- hacks/reset_demo.sh
+++ hacks/reset_demo.sh
@@ -1,2 +1,3 @@
 find demo/configs -name .terraform -exec rm -rf {} \; > /dev/null 2>&1 || true
-find demo/configs -name terraform.tfstate* -exec rm -rf {} \; > /dev/null 2>&1 || true
+find demo/configs -name terraform.tfstate -exec rm {} \; > /dev/null 2>&1 || true
+find demo/configs -name environment -exec rm {} \; > /dev/null 2>&1 || true
