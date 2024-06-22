c7045832e60d7396c3901fb023b1e14160e3e5d0
Louis Garman <louisgarman@gmail.com>
2024-06-22 14:49:44 +0100

    wip

--- internal/app/helpers_test.go
+++ internal/app/helpers_test.go
@@ -39,39 +39,6 @@ func setup(t *testing.T, workdir string, opts ...configOption) *testModel {
 	// network roundtrips to retrieve or query providers.
 	mirrorConfigPath, err := filepath.Abs(mirrorConfigPath)
 	require.NoError(t, err)
-	// Remove all .terraform directories from testdata. These can sometimes be
-	// created outside of tests, e.g. running pug within the repo, but none the
-	// of tests should start with a .terraform directory, otherwise it can lead
-	// to tests falsely passing.
-	{
-		dirs, err := filepath.Glob("./testdata/*/modules/*/.terraform")
-		require.NoError(t, err)
-		for _, dir := range dirs {
-			os.RemoveAll(dir)
-		}
-	}
-	// Remove all .terraform.lock.hcl files from testdata. These can sometimes be
-	// created outside of tests, e.g. running pug within the repo, but none the
-	// of tests should start with a .terraform.lock.hcl directory, otherwise it
-	// can cause unintended consequences.
-	{
-		files, err := filepath.Glob("./testdata/*/modules/*/.terraform.lock.hcl")
-		require.NoError(t, err)
-		for _, file := range files {
-			os.Remove(file)
-		}
-	}
-	// Remove all .terraform-cache directories from testdata. These can sometimes be
-	// created outside of tests, e.g. running pug within the repo, but none the
-	// of tests should start with a .terraform-cache directory, otherwise it
-	// can cause unintended consequences.
-	{
-		dirs, err := filepath.Glob("./testdata/*/modules/*/.terraform-cache")
-		require.NoError(t, err)
-		for _, dir := range dirs {
-			os.RemoveAll(dir)
-		}
-	}
 	// Copy workdir to a dedicated directory for this test, to ensure any
 	// artefacts created in workdir are done so in isolation from other
 	// tests that are run in parallel, and to ensure artefacts don't persist to
