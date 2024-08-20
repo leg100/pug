da9aff7be258ec2dd4a8d6eae59a1f6ea1fb7177
Louis Garman <louisgarman@gmail.com>
2024-08-20 18:38:18 +0100

    wip

--- internal/integration/cost_test.go
+++ internal/integration/cost_test.go
@@ -26,7 +26,6 @@ func TestCost(t *testing.T) {
 
 	// Wait for infracost task to produce overall total
 	waitFor(t, tm, func(s string) bool {
-		t.Log(s)
 		return matchPattern(t, `Task.*cost.*exited`, s) &&
 			matchPattern(t, `OVERALL TOTAL.*\$2\,621\.90`, s)
 	})
@@ -36,7 +35,6 @@ func TestCost(t *testing.T) {
 
 	// Each workspace should now have a cost.
 	waitFor(t, tm, func(s string) bool {
-		t.Log(s)
 		return matchPattern(t, `modules/a.*default.*\$87.116912`, s) &&
 			matchPattern(t, `modules/a.*dev.*\$2360.547736`, s) &&
 			matchPattern(t, `modules/b.*default.*\$87.116912`, s) &&
