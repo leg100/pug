dea486affb70e811e76627b12d89ab998989c9ca
Louis Garman <louisgarman@gmail.com>
2024-08-20 21:24:35 +0100

    wip

--- hacks/reset_demo.sh
+++ hacks/reset_demo.sh
@@ -1,4 +1,5 @@
 find demo/ -name .terraform -exec rm -rf {} \; > /dev/null 2>&1 || true
 find demo/ -name terraform.tfstate -exec rm {} \; > /dev/null 2>&1 || true
 find demo/ -name terraform.tfstate.* -exec rm {} \; > /dev/null 2>&1 || true
+find demo/ -name .terraform.lock.hcl -exec rm {} \; > /dev/null 2>&1 || true
 find demo/ -name environment -exec rm {} \; > /dev/null 2>&1 || true
