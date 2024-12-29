6334abb13b75a41a56f8c00626254d8365f7f2a8
Louis Garman <louisgarman@gmail.com>
2024-12-29 15:13:27 +0000

    wip

---
+++ hacks/reset_demo.sh
@@ -0,0 +1,5 @@
+asdf uninstall terraform > /dev/null 2>&1 || true
+find demo/ -name .terraform -exec rm -rf {} \; > /dev/null 2>&1 || true
+find demo/ -name terraform.tfstate -exec rm {} \; > /dev/null 2>&1 || true
+find demo/ -name .terraform.lock.hcl -exec rm {} \; > /dev/null 2>&1 || true
+find demo/ -name environment -exec rm {} \; > /dev/null 2>&1 || true
