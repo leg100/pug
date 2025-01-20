ab77cc9612f9e126e09808601d66f504a635705f
Louis Garman <louisgarman@gmail.com>
2025-01-20 09:59:00 +0000

    wip

--- internal/task/buffer.go
+++ internal/task/buffer.go
@@ -1,19 +1,31 @@
 package task
 
 import (
-	"bytes"
 	"io"
 	"sync"
 )
 
+// buffer captures task output and makes it available to multiple readers,
+// providing the following functionality:
+//
+// * readers receive all of the output, from the beginning of the task through
+// to its termination.
+// * block reader if there is nothing more to read but the task hasn't yet
+// finished.
 type buffer struct {
 	buf     []byte
-	clients []*io.PipeWriter
+	clients []*bufferClient
 	mu      sync.Mutex
+
+	// more signals when there is further task output and when the task has
+	// finished.
+	more chan struct{}
 }
 
 func newBuffer() *buffer {
-	return &buffer{}
+	return &buffer{
+		more: make(chan struct{}, 1),
+	}
 }
 
 func (b *buffer) Write(p []byte) (int, error) {
@@ -21,30 +33,68 @@ func (b *buffer) Write(p []byte) (int, error) {
 	defer b.mu.Unlock()
 
 	b.buf = append(b.buf, p...)
-
 	for _, client := range b.clients {
-		_, _ = io.Copy(client, bytes.NewReader(p))
+		client.mu.Lock()
+		client.buf = append(client.buf, p...)
+		client.mu.Unlock()
+
+		// inform client there is more output
+		select {
+		case client.more <- struct{}{}:
+		default:
+		}
 	}
 	return len(p), nil
 }
 
 // NewReader returns a copy of the buffer to read from.
 func (b *buffer) NewReader() io.Reader {
-	r, w := io.Pipe()
+	client := &bufferClient{
+		buf:  b.buf,
+		more: b.more,
+	}
 
 	b.mu.Lock()
-	b.clients = append(b.clients, w)
-	b.mu.Unlock()
+	defer b.mu.Unlock()
 
-	go func() {
-		_, _ = io.Copy(w, bytes.NewReader(b.buf))
-	}()
+	b.clients = append(b.clients, client)
 
-	return r
+	return client
 }
 
 func (b *buffer) Close() {
-	for _, client := range b.clients {
-		_ = client.Close()
+	close(b.more)
+}
+
+type bufferClient struct {
+	buf    []byte
+	offset int
+	// mu guards access to buf
+	mu   sync.Mutex
+	more chan struct{}
+}
+
+func (c *bufferClient) Read(p []byte) (int, error) {
+	if n := c.read(p); n > 0 {
+		return n, nil
 	}
+	_, ok := <-c.more
+	if n := c.read(p); n > 0 {
+		return n, nil
+	}
+	if ok {
+		return 0, nil
+	} else {
+		// TODO: handle case of len(p) == 0
+		return 0, io.EOF
+	}
+}
+
+func (c *bufferClient) read(p []byte) int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	n := copy(p, c.buf[c.offset:])
+	c.offset += n
+	return n
 }
--- internal/task/buffer_test.go
+++ internal/task/buffer_test.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,37 +21,42 @@ func TestBuffer_NewReader(t *testing.T) {
 	r1 := buf.NewReader()
 	r2 := buf.NewReader()
 
-	got := make([]byte, len("hello world"))
-
-	_, err = r1.Read(got)
+	got, err := io.ReadAll(r1)
 	require.NoError(t, err)
 	assert.Equal(t, "hello world", string(got))
 
-	_, err = r2.Read(got)
+	got, err = io.ReadAll(r2)
 	require.NoError(t, err)
 	assert.Equal(t, "hello world", string(got))
 }
 
-//func TestBuffer_Stream(t *testing.T) {
-//	t.Parallel()
-//
-//	buf := newBuffer()
-//	ch := buf.Stream()
-//
-//	_, err := buf.Write([]byte("hello"))
-//	require.NoError(t, err)
-//
-//	got := <-ch
-//	assert.Equal(t, "hello", string(got))
-//
-//	_, err = buf.Write([]byte("world"))
-//	require.NoError(t, err)
-//
-//	got = <-ch
-//	assert.Equal(t, "world", string(got))
-//
-//	buf.Close()
-//
-//	got = <-ch
-//	assert.Nil(t, got)
-//}
+func TestBuffer_Stream(t *testing.T) {
+	t.Parallel()
+
+	buf := newBuffer()
+	r1 := buf.NewReader()
+
+	_, err := buf.Write([]byte("hello"))
+	require.NoError(t, err)
+
+	p := make([]byte, 5)
+	_, err = r1.Read(p)
+	require.NoError(t, err)
+
+	assert.Equal(t, "hello", string(p))
+
+	_, err = buf.Write([]byte(" world"))
+	require.NoError(t, err)
+
+	p = make([]byte, 6)
+	_, err = r1.Read(p)
+	require.NoError(t, err)
+
+	assert.Equal(t, " world", string(p))
+
+	buf.Close()
+
+	got, err := io.ReadAll(r1)
+	assert.Equal(t, 0, len(got))
+	assert.NoError(t, err)
+}
